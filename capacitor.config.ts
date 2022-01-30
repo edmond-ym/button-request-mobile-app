20def49e7c4dc8887e49956dc2fda357902bd44c
Edmond <edmond@inchoatae.com>
2022-01-30 17:56:12 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Button Request',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
